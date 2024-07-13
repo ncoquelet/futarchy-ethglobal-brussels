f1f80e932379773b379f31ee25c3dad92658bf99
nada-r <nadarzer@gmail.com>
2024-07-13 14:10:50 +0200

    setup tailwind and fonts

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
