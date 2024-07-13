53194e279e5461371418aad019e8481d5c670bcf
Romain <romain.lafont1@gmail.com>
2024-07-13 22:17:20 +0200

    -

--- frontend/src/app/config.tsx
+++ frontend/src/app/config.tsx
@@ -5,5 +5,6 @@ config();
 
 export const ALCHEMY_KEY = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
 export const WALLETCONNECT_KEY = process.env.NEXT_PUBLIC_WALLETCONNECT_KEY;
-export const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-export const ABI = process.env.ABI;
+export const GOVERNANCE_CONTRACT_ADDRESS = process.env.GOVERNANCE_CONTRACT_ADDRESS;
+export const GOVERNANCE_CONTRACT_ABI = process.env.GOVERNANCE_CONTRACT_ABI;
+export const GOAL_CONTRACT_ABI = process.env.GOAL_CONTRACT_ABI;
