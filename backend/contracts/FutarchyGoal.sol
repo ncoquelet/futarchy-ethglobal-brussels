9f407a2347f0a1dae77a058ebb032ffeb0b06f26
Romain <romain.lafont1@gmail.com>
2024-07-13 12:38:09 +0200

    Add constructor to FutarchyGoal, stock owner address in goal

--- backend/contracts/FutarchyGoal.sol
+++ backend/contracts/FutarchyGoal.sol
@@ -4,33 +4,39 @@ pragma solidity 0.8.26;
 import {FutarchyProposal} from "./FutarchyProposal.sol";
 import {FutarchyOracle} from "./FutarchyOracle.sol";
 
-contract FutarchyGoal {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract FutarchyGoal is Ownable {
   address[] proposals;
   address oracle;
+  string description;
 
   event ProposalAdded(uint _proposalId, address _proposalAddr, address _oracleAddress);
 
-  constructor(string memory _description) {}
+  constructor(string memory _description, address _owner) Ownable(_owner) {
+    description = _description;
+    oracle = address(new FutarchyOracle());
+  }
 
-  function createProposal(string calldata _description) public {
+  function createProposal(string calldata _description) public onlyOwner() {
     address proposal = address(new FutarchyProposal(_description));
     proposals.push(proposal);
     uint proposalId = proposals.length - 1;
 
-    oracle = address(new FutarchyOracle());
-
-    emit ProposalAdded(proposalId, proposal, oracle);
+    emit ProposalAdded(proposalId, proposal);
   }
 
   function nbProposals() external view returns (uint256) {
     return proposals.length;
   }
 
-  function getProposal(uint _goalId) external view returns (address) {
-    return proposals[_goalId];
+  function getProposal(uint _proposalId) external view returns (address) {
+    return proposals[_proposalId];
   }
 
   function getGoalOracle() external view returns (address) {
     return oracle;
   }
+
 }
