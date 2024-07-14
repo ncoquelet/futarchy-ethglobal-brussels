c34ed182ea3abbbb53fc7bc5447e566ad8e675a5
Matthieu SCELLES <matthieu@alygne.me>
2024-07-14 03:11:52 +0200

    feat: IPFS

--- frontend/src/app/config.tsx
+++ frontend/src/app/config.tsx
@@ -8,3 +8,4 @@ export const WALLETCONNECT_KEY = process.env.NEXT_PUBLIC_WALLETCONNECT_KEY;
 export const GOVERNANCE_CONTRACT_ADDRESS =
   process.env.NEXT_PUBLIC_GOVERNANCE_CONTRACT_ADDRESS;
 export const FROM_BLOCK = process.env.NEXT_PUBLIC_FROM_BLOCK;
+export const IPFS_API_KEY = process.env.IPFS_API_KEY;
